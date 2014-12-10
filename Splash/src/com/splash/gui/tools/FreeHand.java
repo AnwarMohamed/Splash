dfc37d2e0e93c9574a6a7be148c49cd03cd89847
AnwarMohamed <anwarelmakrahy@gmail.com>
2014-12-10 16:40:05 +0200

    Adding License

--- Splash/src/com/splash/gui/tools/FreeHand.java
+++ Splash/src/com/splash/gui/tools/FreeHand.java
@@ -1,7 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Copyright (C) 2014
+ *                      Anwar Mohamed     <anwarelmakrahy@gmail.com>
+ *                      Abdallah Elerian  <abdallah.elerian@gmail.com>
+ *                      Moataz Hammouda   <moatazhammouda4@gmail.com>
+ *                      Yasmine Elhabashi <yasmine.elhabashi@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to authors.
+ *
  */
 package com.splash.gui.tools;
 
@@ -10,24 +26,20 @@ import java.awt.Graphics;
 import java.awt.Shape;
 import java.awt.geom.Line2D;
 
-/**
- *
- * @author Abdallah
- */
 public class FreeHand extends Tool {
 
     public FreeHand(int x, int y) {
         super();
     }
-    
-    public FreeHand(){
-        
+
+    public FreeHand() {
+
     }
 
     public void paint(Graphics g) {
         super.paint(g);
         Shape drawPoint = new Line2D.Float(
-                getX(), getY(),getX(),getY());
+                getX(), getY(), getX(), getY());
         graph.draw(drawPoint);
     }
 
